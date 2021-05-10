5cbd4e9b6e7fc2d911f13d333fb7444ce68e2d25
WhiteAngel1331 <whitecorp1331@gmail.com>
2021-05-10 13:14:14 -0400

    primeiraVersao

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 var inputTXID = document.getElementById("TXID")
 var botao = document.getElementById("submitTXID")
-var tagMain = document.querySelector("main")
+var main = document.querySelector(".main")
 var modal = document.querySelector(".modalBackground")
 var span = document.querySelector(".modalBackground span")
-
+var tokenSpace = document.getElementById("TokenSpace")
+var tx1Space = document.getElementById("TX1Space")
+var addSpace = document.getElementById("ADDSpace")
+ 
 botao.addEventListener("click", async function(){
    try {
         var conteudoJSON = await requestBlock(inputTXID.value)
@@ -19,12 +22,16 @@ botao.addEventListener("click", async function(){
     if(conteudoJSON.isImage){
         let imagem = document.createElement("img")
         imagem.src = conteudoJSON.Content
-        tagMain.appendChild(imagem)
+        main.append(imagem)
     }else{
         let texto = document.createElement("p")
         texto.textContent = conteudoJSON.Content
-        tagMain.appendChild(texto)
+        main.appendChild(texto)
     }
+
+    tokenSpace.textContent += conteudoJSON.Token
+    tx1Space.textContent += conteudoJSON.TX1
+    addSpace.textContent += conteudoJSON.ADD
 
     modal.style.display="none"
 
